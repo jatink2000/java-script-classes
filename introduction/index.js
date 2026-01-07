ca5c70702407938d394630add47fc934e9593a67
jatink2000 <jatink8059@gmail.com>
2026-01-07 14:37:07 +0530

    hf

---
+++ introduction/index.js
@@ -0,0 +1,7 @@
+    // 1. console : 
+       /* console.log("hello js")
+        console.warn("hyy")
+        console.error("hiii")  */
+
+    // 2. body 
+        document.writeln("hello java script")
---
+++ variables/variables.js
@@ -0,0 +1,95 @@
+// variables : variables is a container . which is used to store the data/value . 
+
+
+// how to create the variable in js : to create the variable , first we define the variable name and then store the data /value . 
+
+// syntex : 
+// variablename = data/value . 
+
+// e.g : 
+
+// students=12 
+// apples=29 
+
+
+// how to access the variables : to access the variables , use the variable name inside the print function . 
+// document.writeln(students) // display a variable 
+// document.writeln("students") // display a msg 
+// console.log(apples)
+
+
+// general rules of a variables : 
+// 1. variables name can start with letter or underscore (_). 
+        // _groot=47
+        // groot=46
+        // document.writeln(_groot)
+        // document.writeln(groot)
+
+// 2. variables name cannot start with number . 
+        // 1groot=48   
+        // g2ro5ot6=48 
+
+// 3. variables name cannot contain any symbol only $ symbol is used to create the variable . 
+        // gr$oot=48
+
+// 4. variables name are case senstive . 
+        // groot=48
+        // document.writeln(groot)
+
+
+// 5. variables name cannot contain extra space .
+        // groot academy=47
+
+
+
+
+// how to declare the variable in java script : 
+// 1. var : var is used to redeclare the variables . 
+
+// var groot=47
+// document.writeln(groot)
+
+
+
+
+// var groot=49
+// document.writeln(groot)
+
+
+
+// 2. let : let is used to redeclare the variables but inside the block scope .  
+
+// let a=38
+// // document.writeln(a) // 38
+
+// a=10
+// document.writeln(a)
+
+// {
+//     let a=48
+//     document.writeln(a) // 48
+// }
+
+
+
+// 3. const : const is used to create the constant variable . it means cannot be changed  . 
+
+// const a=38
+
+// a=20
+
+// document.writeln(a)
+
+
+
+a=29
+b=95
+
+x=a 
+a=b 
+b=x 
+document.writeln(a)
+document.writeln(b)
+
+
+
