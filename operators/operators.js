f9b03abbf4ac0fd72109a5b0112b8be2a2d3f994
jatink2000 <jatink8059@gmail.com>
2026-01-08 14:49:46 +0530

    fj

---
+++ operators/operators.js
@@ -0,0 +1,74 @@
+
+
+// operators : operators is a symbol . which is used to perform operation between variables and value . 
+// types of operators : 
+// there are 4 types of operators : 
+
+// 1. arithmatic operators : the arithmatic operators is used to perfrom the mathematics task . 
+    // symbol : +,-,*,/,% (modules)
+
+    // let a=13
+    // let b=5
+    // document.writeln(a+b)
+    // document.writeln(a-b)
+    // document.writeln(a*b)
+    // document.writeln(a/b) // 2.6
+    // document.writeln(a%b) // 3
+
+
+    // i. unray : 
+        // 1. pre : increment / decrement
+        // let a=4
+        // document.writeln(++a)  // 5 
+        // document.writeln(--a) // 4
+
+
+        // 2. post : increment / decrement
+
+        // let a=4
+        // document.writeln(a--) // 4 : 3
+        // document.writeln(++a) // 4 : 4
+        // document.writeln(a++) // 4 : 5 
+        // document.writeln(a) //  5
+
+
+
+// 2. assignment : the assignment operators is used to perfrom assign the variables and value .   
+    // symbol : =,+=,-=,*=,/=,%= 
+
+    // let a=8
+    // a-=3 // 8+=3 : 11
+    // document.writeln(5)
+
+
+
+// 3. comparision : the comparision operators is used to compair the variabels and value . and return the boolean data (true /false). 
+        // symbol : ==,<,>,<=,>=,!= (not equal to)
+
+        // let a=26
+        // let b=26
+        // document.writeln(a==b) // true 
+        // document.writeln(a<b) // false 
+        // document.writeln(a>b) // false 
+        // document.writeln(a<=b) // true 
+        // document.writeln(a>=b) // true 
+        // document.writeln(a!=b) // false 
+
+
+
+// 4. logical : the logical opertaors is used to logics . 
+    // 1. and (&&) : when all conditions is true then return true . otherwise it return false  . 
+
+    // let a=58
+    // let b=58
+    // document.writeln(a<=b && a>=b && a>=b)
+
+
+    // 2. or (||) : when anyone conditions is true then return true . otherwise it return false  . 
+
+
+    // let a=58
+    // let b=58
+    // document.writeln(a<b || a>b || a>=b)
+
+
