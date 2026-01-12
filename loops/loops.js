be8aa1cdc3bc718bcf90c5d4a801c6d171c34c92
jatink2000 <jatink8059@gmail.com>
2026-01-12 16:08:06 +0530

    k

---
+++ loops/loops.js
@@ -0,0 +1,80 @@
+
+
+
+// loops :repeataion the statment till the condition is true .
+
+// start , conditon/end , increment /decrement 
+
+// types of loop :
+// 1. entry loop  : check the condition when enter the loop . 
+        // 1. for loop : 
+        // syntex : 
+        // for(start ;conditon/end ;increment /decrement ){
+        //     code to be excute 
+        // }
+
+
+        // for(i=1;i<=100;i++){
+        //     document.writeln(i)
+        // }
+
+
+
+        // 2. while loop : 
+        // syntex :
+        // start 
+        // while(condition){
+        //     code to be excute 
+        //     increment /decrement
+        // }
+
+        // i=100
+        // while(i>=1){
+        //     document.writeln(i)
+        //     i--
+        // }
+
+
+
+
+// 2. exit loop : check the condition when exit the loop .
+        // note : runs one time . if condition is false . 
+
+
+        // 1. do while loop : 
+        // syntex : 
+        // start 
+        // do{
+        //     code to be excute 
+        //     increment /decrement
+        // }while(condition)
+
+
+
+        // i=1
+        // do{
+        //     document.writeln("hello")
+        //     i++
+        // }while(i>=10)
+
+
+
+
+        // increase two numbers : 
+        // for(i=0;i<=50;i+=3){
+        //     document.writeln(i)
+        // }
+
+
+
+        // loops with control statment/ conditions : 
+        // i=1
+        // while(i<=5000){
+        //     // control statment 
+        //     if(i%40==0){
+        //         document.writeln(i)
+        //     }
+        //     i++
+        // }
+
+
