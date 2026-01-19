c554f8e8fe178c5ea14106b37845c5f0b9bcfc6a
jatink2000 <jatink8059@gmail.com>
2026-01-19 16:12:13 +0530

    sj

---
+++ data types/datatypes.js
@@ -0,0 +1,52 @@
+
+
+// data types : a java script variable can hold different types of data . and these data's type is known as data types . 
+
+
+
+// 1. primitive : 
+// 1. number : it can represent the numaric value . 
+// let a=28
+// let b=5.6 
+
+
+
+
+// 2. string : 
+// let a="hello"
+// let b='h' 
+
+
+
+// 3. boolean :  true /false 
+// let a=true 
+// let b=false 
+
+
+// 4. undefined : 
+// let a;
+// document.writeln(a)
+
+
+// 5. null :
+// let a=null 
+// document.writeln(a)
+
+
+
+
+// 2. non-primitive : 
+
+// 1. array : 
+// let a=[2,3,49,4,3,2]
+
+
+// 2. object : 
+// let students={name:"jatin",city:"jaipur"}
+
+
+
+// 3. function : 
+// function groot(){
+//     code to be excute 
+// }
--- string/string.js
+++ string/string.js
@@ -77,7 +77,94 @@
 
 
 // 4. touppercase : 
+// let a="hello groot"
+// b=a.toUpperCase()
+// document.writeln(b)
 
-let a="hello groot"
-b=a.toUpperCase()
-document.writeln(b)
+
+// 5. tolowercase: the tolowercase method is used to convert the lower case of a string . 
+
+// let a="HELLO"
+// b=a.toLowerCase()
+// document.writeln(b)
+
+
+
+// 6. slice(): the slice method is used to select the element of a string . 
+// slice(start,end)
+// note : end not count . 
+
+
+// let a="hello groot academy"
+// let b=a.slice(5,13)
+// document.writeln(b)
+
+
+// 7. substring : the substring method is used to select the element of a string . 
+// substring(start,end)
+// note : end not count . 
+
+// let a="hello groot academy"
+// let b=a.substring(5,13)
+// document.writeln(b)
+
+
+// 8. concat() : the concat method is used to marge the two strings . 
+
+// let a="hello "
+// let b="groot "
+// let c=b.concat(a)
+// document.writeln(c)
+
+
+
+// 9. trim(): the trim method is used to remove the extra space in string from both sides . 
+
+// let a="        hello      "
+// let c=a.trim()
+// document.writeln(c.length)
+
+
+// 10. trimstart(): the trimstart method is used to remove the extra space in string from left side . 
+
+// let a="        hello      "
+// let c=a.trimStart()
+// document.writeln(c.length)
+
+
+// 10. trimend(): the trimend method is used to remove the extra space in string from right side . 
+// let a="        hello      "
+// let c=a.trimEnd()
+// document.writeln(c.length)
+
+
+// 11. indexof : the indexof method is used to find the index number of an element . 
+// let a="software"
+// let b=a.indexOf("e")+1
+// document.writeln(b)
+
+
+// 12. search(): the search method is used to find the index number of an element . 
+// let a="software"
+// let b=a.search("e")
+// document.writeln(b)
+
+
+// 13. includes: the includes method is used to check the element present in a string . 
+
+// let a="software"
+// let b=a.includes("of")
+// document.writeln(b)
+
+
+// 14. startswith : the startswith method is used to check the element present in a string from start . 
+
+// let a="software"
+// let b=a.startsWith("so")
+// document.writeln(b)
+
+
+// 15. endswith : the endswith method is used to check the element present in a string from last . 
+// let a="software"
+// let b=a.endsWith("re")
+// document.writeln(b)
