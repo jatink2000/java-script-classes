2959cf4bba437ab4b612648c802d51679ca8061c
jatink2000 <jatink8059@gmail.com>
2026-01-23 16:13:56 +0530

    hd

--- functions/function.js
+++ functions/function.js
@@ -56,17 +56,121 @@
 // function return :you can pass the data or information outside the function . use as a return .
 
 
+// function groot(){
+//     let a=30
+//     return(a)
+// }
+
+// let x=groot()
+// document.writeln(x)
 
 
 
 // local scope and global scope : 
 
-let a=69 // global variable 
+// let a=69 // global variable 
+// function groot(){
+//     // let a=48 // local variable 
+//     document.writeln(a) // 48 
+// }
+// document.writeln(a) // 69 
+// groot()
 
-function groot(){
-    // let a=48 // local variable 
-    document.writeln(a) // 48 
-}
-document.writeln(a) // 69 
 
-groot()
+
+
+// types of structure of a function : 4 
+// 1. no return no argument 
+// function groot(){
+//     document.writeln("hello groot")
+// }
+// groot()
+
+
+
+
+// 2. no return with argument  : 
+
+// function groot(name){
+//     document.writeln(name)
+// }
+// groot("hello groot")
+
+
+
+// 3. with return no argument 
+
+
+// function groot(){
+//     let a="hello groot"
+//     return(a)
+// }
+// let x=groot()
+// document.writeln(x)
+
+
+// 4. with return with argument 
+
+// function groot(a){
+//     return(a)
+// }
+// let x=groot("hello groot")
+// document.writeln(x)
+
+
+
+
+// default parameter : the default parameter is used to if no argument is provided . 
+
+
+// function groot(a,b=38){
+//     document.writeln(a)
+//     document.writeln(b)
+// }
+
+// groot(34,20)
+
+
+
+// types of function : 
+// 1. anonymous function: an anonymous function  is that function that does not have a function name . unlike named function , which are declared with the function keyword using the () and then start the function body {}. 
+
+// syntex : 
+// function (){
+//     code to be excute . 
+// }
+
+// let groot=function (){
+//     document.writeln("hello")
+// }
+
+// // call 
+// groot()
+
+
+// 2. arrow function : in an arrow function , the function keyword not declare . which is declare the function name using the = and the ()followed by the => and the start the function body {}. 
+
+// groot=()=>{
+//     code to be excute 
+// }
+
+
+// groot=()=>{
+//     document.writeln("hello groot")
+// }
+
+// groot()
+
+
+
+// 3. self invoked function : the self invoked function are javascript functions that excute immeditely as they are defined . to define the self invoked function , you can ancloes the anonymous . 
+
+
+// (function (){
+//     code to be excute 
+// })()
+
+
+// (function (){
+//     document.writeln("hello")
+// })()
